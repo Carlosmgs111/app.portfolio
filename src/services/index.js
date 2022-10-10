2717f067e14e3797d4c983c6c51dae409839c283
Carlos <carlosmgs111@outlook.com>
2022-10-10 13:16:19 -0500

    [added] github workflow

--- src/services/index.js
+++ src/services/index.js
@@ -1,9 +1,5 @@
 import config from "../config/config";
-const URL = config.devUrl || "https://graphenum.herokuapp.com";
-const URL_API = config.devUrlApi || "https://graphenum.herokuapp.com/api";
-const URL_GRAPHQL =
-  config.devUrlGraphql || "https://graphenum.herokuapp.com/graphql";
-const URL_SYNAPSE =
-  config.devUrlSynapse || "https://graphenum.herokuapp.com/synapse";
+const URL = config.devUrl || "https://core-blogfolio.herokuapp.com/";
+const URL_API = config.devUrlApi || "https://core-blogfolio.herokuapp.com/api/v1";
 
-export { URL, URL_API, URL_GRAPHQL, URL_SYNAPSE };
+export { URL, URL_API };
