3c036898400111db178e3e8d92d7a35fadcd926b
Carlos Muñoz <carlosmgs111@outlook.com>
2024-02-17 14:41:13 -0500

    [ADDED] hero section and Typing component

---
+++ src/components/Typing/index.tsx
@@ -0,0 +1,10 @@
+import styles from "./styles.module.css";
+
+export const Typing = () => {
+  return (
+      <div className={styles.animated_text}>
+        <span></span>
+        {"  Developer"}
+      </div>
+  );
+};
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,23 +1,11 @@
 import styles from "./styles.module.css";
-
+import { Typing } from "../../components/Typing";
 
 export function Home() {
   return (
     <div className={styles.container}>
-      <article className={styles.section}>
-        <p className={styles.text}>
-          Anim ex nostrud exercitation exercitation anim aliqua. Sit Lorem id
-          laborum proident eu ad Lorem culpa. Occaecat ipsum exercitation
-          consequat aute ullamco. Commodo enim voluptate aliqua pariatur ipsum
-          tempor enim velit nisi. Officia ut adipisicing veniam enim esse aliqua
-          minim Lorem esse amet ut elit ullamco et. Id reprehenderit cillum
-          mollit qui esse. Do sint dolor sunt adipisicing est eu tempor proident
-          sunt commodo ipsum occaecat tempor incididunt. Dolore consectetur
-          nostrud sunt aliquip eiusmod velit. Irure mollit duis sint dolore.
-          Aliqua quis amet est minim occaecat ipsum laboris do laboris minim
-          nostrud anim. Qui adipisicing velit cupidatat adipisicing Lorem.
-          Aliquip duis elit deserunt ad. Aute id ut tempor duis dolor anim.
-        </p>
+      <article className={styles.section.concat(" ", styles.hero)}>
+        <Typing />
       </article>
       <article className={styles.section.concat(" ", styles.even)}>
         <p className={styles.text}>
