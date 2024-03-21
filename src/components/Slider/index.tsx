9a7b9cc800121e9ed7896cdc9577b6b9c832f536
Carlos Muñoz <carlosmgs111@outlook.com>
2024-03-20 19:59:14 -0500

    [UPDATED] to_right css rules added to Slider component in case it is activated

--- src/components/Slider/index.tsx
+++ src/components/Slider/index.tsx
@@ -1,12 +1,12 @@
 import styles from "./styles.module.css";
 import { Children, cloneElement } from "react";
-export const Slider = ({ children }: any) => {
+export const Slider = ({ children, toRight = false }: any) => {
   const slides: any = Children.toArray(children).map((child: any) => (
     <div className={styles.slide}>{cloneElement(child)}</div>
   ));
   return (
     <div className={styles.container}>
-      <div className={`${styles.slider} ${styles.to_right}`}>
+      <div className={`${styles.slider} ${toRight ? styles.to_right : ""}`}>
         {slides}
         {slides}
       </div>
