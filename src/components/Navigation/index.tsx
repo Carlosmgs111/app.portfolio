4b0ec65d089575020a8fbc1375dbc3101795979e
Carlos Muñoz <carlosmgs111@outlook.com>
2024-05-22 20:03:09 -0500

    [ADDED] Linkdex, [UPDATED] Navigation component

--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -1,26 +1,50 @@
-import { Link as LinkRouter } from "react-router-dom";
 import styles from "./styles.module.css";
-import { useLocation } from "react-router-dom";
 import { useToggle } from "../../hooks/useToggle";
 import { LogoSVG } from "./../../icons";
 import { useStateValue } from "../../contexts/context";
 import { actionTypes } from "../../";
+import { Linkdex } from "./Linkdex";
+import { useRef, useEffect } from "react";
+import { useLocation } from "react-router-dom";
 
 export default function Navigation({ className, login, pages }: any) {
-  const location = useLocation();
   const [{ token, avatar }, dispatch] = useStateValue();
+  const { pathname } = useLocation();
+  const referencesRefs: any = useRef({});
   const [menu, switchMenu] = useToggle(
     { show: false, name: "fas fa-bars p-2 item" },
     { show: true, name: "fas fa-times p-2 item" }
   );
+  const indicatorRef: any = useRef(null);
+
+  useEffect(() => {
+    const currentPath = referencesRefs.current[pathname.split("/")[1]];
+    if (!currentPath) {
+      indicatorRef.current.style.scale = "0";
+      indicatorRef.current.style.opacity = "0";
+      return;
+    }
+    const { offsetLeft, offsetWidth, offsetHeight, offsetTop } =
+      currentPath.current;
+    indicatorRef.current.style.scale = "1";
+    indicatorRef.current.style.opacity = "1";
+    indicatorRef.current.style.left = `${offsetLeft}px`;
+    indicatorRef.current.style.width = `${offsetWidth}px`;
+    indicatorRef.current.style.top = `${offsetTop}px`;
+    indicatorRef.current.style.height = `${offsetHeight}px`;
+  }, [pathname]);
 
   return (
     <div className={styles.navbar_container}>
       <div className={`${className} ${styles.navbar}`}>
         <div className={styles.navbar_header}>
-          <LinkRouter onClick={() => menu.show && switchMenu()} to="/">
+          <Linkdex
+            className={styles.banner}
+            onClick={() => menu.show && switchMenu()}
+            to=" "
+          >
             <LogoSVG></LogoSVG>
-          </LinkRouter>
+          </Linkdex>
           <i
             className={`${menu.name} ${styles.button}`}
             id="nav-button"
@@ -33,41 +57,48 @@ export default function Navigation({ className, login, pages }: any) {
               menu.show ? styles.show : ""
             }`}
           >
+            <span
+              ref={indicatorRef}
+              id="navbar_indicator"
+              className={styles.indicator}
+            ></span>
             {pages &&
               pages.map(({ to, label }: any, index: any) => {
+                referencesRefs.current[to] = { current: null };
+                const linkRef = referencesRefs.current[to];
                 return (
-                  <LinkRouter
-                    className={`${styles.link} ${
-                      to === location.pathname.replace("/", "") &&
-                      styles.selected
-                    }`}
-                    type="button"
+                  <Linkdex
+                    linkRef={linkRef}
                     key={index}
                     to={to}
                     id={index}
-                    onClick={switchMenu}
+                    onClick={() => menu.show && switchMenu()}
                   >
                     {label}
-                  </LinkRouter>
+                  </Linkdex>
                 );
               })}
             {login && (
-              <LinkRouter
-                className={` ${styles.link} ${styles.avatar}`}
-                to={`/${login.to}`}
+              <Linkdex
+                className={styles.avatar}
+                to={login.to}
                 onClick={!token && login?.onClick}
               >
                 {token ? (
-                  <img src={avatar} onClick={()=>menu.show && switchMenu()} alt="Profile user avatar" />
+                  <img
+                    src={avatar}
+                    onClick={() => menu.show && switchMenu()}
+                    alt="Profile user avatar"
+                  />
                 ) : (
                   "Login"
                 )}
-              </LinkRouter>
+              </Linkdex>
             )}
           </ul>
         </nav>
       </div>
-      <div className={styles.page_settings}>
+      <div className={`${styles.page_settings}  ${menu.show && styles.show}`}>
         <select
           onChange={(e) =>
             dispatch({
