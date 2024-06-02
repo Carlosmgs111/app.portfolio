e75692b72b19e9dd6363e2b2d7711b894e29d3fa
Carlos Muñoz <carlosmgs111@outlook.com>
2024-06-02 00:18:26 -0500

    [HOTFIX] delete behavior in Project container, [UPDATED] SocketService

--- src/services/index.ts
+++ src/services/index.ts
@@ -5,10 +5,6 @@ export const URL = config.devUrl || config.prodUrl;
 export const URL_API =
   config.devUrlApi || `${config.prodUrl}/api/${config.prodApiVersion}`;
 
-export const URL_WS =
-  config.devUrl?.replace(":7080", ":7081") ||
-  config.prodUrl?.replace("/", ":7081");
-
 export const SocketService = new SS(/* [
   { imageService: "http://127.0.0.1:7081" },
-] */).addClient({ imageService: URL_WS });
+] */).addClient({ imageService: "http://127.0.0.1:7081" });
