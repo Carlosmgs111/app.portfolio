4f48a4056881222ae02a7b87a06e7e727b5d8229
Carlos Muñoz <carlosmgs111@outlook.com>
2024-06-13 04:45:55 -0500

    [FIXED] Multisidebar size (height) adaptation behavior when wrapped element come to end

--- src/components/Image/index.tsx
+++ src/components/Image/index.tsx
@@ -14,7 +14,9 @@ export const Image = ({
   useEffect(() => {
     let tries = 0;
     const maxTries = 3;
+    if (!ref.current) return;
     ref.current.addEventListener("error", () => {
+      if (!ref.current) return;
       if (++tries < maxTries)
         ref.current.src =
           "https://github.com/Carlosmgs111/assets-public/assets/41123597/c8364209-d4f5-4930-a782-e9d51f286511";
