e0a14308b17828a2a39992a414c38457dbe27050
Carlos Muñoz <carlosmgs111@outlook.com>
2024-06-23 00:09:30 -0500

    [UPDATED] InfiniteCarousel component

---
+++ src/components/InfiniteCarousel/index.tsx
@@ -0,0 +1,68 @@
+import styles from "./styles.module.css";
+import { manyfy } from "../../utils";
+import { Children, cloneElement, useRef, useEffect } from "react";
+export const Slider = ({
+  children,
+  toRight = false,
+  pausable = true,
+  timing = 10,
+}: any) => {
+  const sliderRef: any = useRef(null);
+  const containerRef: any = useRef(null);
+  const slidesLength = children.length;
+  const slides: any = Children.toArray(children).map(
+    (child: any, index: any) => (
+      <div key={index} className={styles.slide}>
+        {cloneElement(child)}
+      </div>
+    )
+  );
+  const baseFactor = 4;
+  useEffect(() => {
+    const slider = sliderRef.current;
+    const container = containerRef.current;
+    if (!slider || !container) return;
+    let prevContainerScrollLeft = 1;
+    container.style.setProperty("--timing", `${timing * baseFactor}s`);
+
+    const handleScroll = () => {
+      if (
+        container.scrollLeft > prevContainerScrollLeft &&
+        container.scrollLeft >= slider.offsetWidth / baseFactor
+      ) {
+        prevContainerScrollLeft = -1;
+        container.scrollLeft = 0;
+        return;
+      }
+      if (
+        container.scrollLeft < prevContainerScrollLeft &&
+        container.scrollLeft === 0
+      ) {
+        prevContainerScrollLeft = slider.offsetWidth / baseFactor + 1;
+        container.scrollLeft = slider.offsetWidth / baseFactor;
+        return;
+      }
+      prevContainerScrollLeft = container.scrollLeft;
+    };
+    container.scrollLeft = prevContainerScrollLeft;
+    container.addEventListener("scroll", handleScroll);
+    return () => container.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  return (
+    <div ref={containerRef} className={styles.container}>
+      <div
+        ref={sliderRef}
+        className={`
+        ${styles.slider} ${styles.animated} ${toRight ? styles.to_right : ""} 
+        ${pausable ? styles.pausable : ""}`}
+      >
+        {manyfy(slides, baseFactor)}
+        {slidesLength < 4 && manyfy(slides, baseFactor)}
+        {slidesLength < 4 && manyfy(slides, baseFactor)}
+        {slidesLength < 6 && manyfy(slides, baseFactor)}
+        {slidesLength < 6 && manyfy(slides, baseFactor)}
+      </div>
+    </div>
+  );
+};
