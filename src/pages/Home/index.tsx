dba08382eab5bcf11148b046e18ff4cab4e42836
Carlos Muñoz <carlosmgs111@outlook.com>
2024-11-08 14:57:28 -0500

    [ADDED] COA component Coat Of Arms

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,8 +1,10 @@
 import styles from "./styles.module.css";
+import { COA } from "../../components/COA";
 
 export function Home({}: any) {
   return (
     <div className={styles.page}>
+      <COA/>
     </div>
   );
 }
