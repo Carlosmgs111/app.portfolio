f7181a012025a64a4bab372c20d00f3bf6397b6b
Carlos Muñoz <carlosmgs111@outlook.com>
2024-11-11 05:59:39 -0500

    [REMOVED] Video as bg in hero

--- src/components/AppBackground/index.tsx
+++ src/components/AppBackground/index.tsx
@@ -1,7 +1,6 @@
 import styles from "./styles.module.css";
 import { useLocation } from "react-router-dom";
 import { COA } from "../COA";
-import { Video } from "../Video";
 
 export const AppBackground = () => {
   const { pathname } = useLocation();
@@ -11,19 +10,6 @@ export const AppBackground = () => {
       <div className={styles.foreground}>
         <COA />
       </div>
-      <section className={`${styles.hero}`}>
-        <Video
-          className={`${styles.video}`}
-          src={
-            "https://github.com/user-attachments/assets/a4adcad9-67cb-4573-9b72-253f2dac23b4"
-          }
-          pause={pathname !== "/"}
-          autoPlay
-          muted
-          loop
-          playsInline
-        ></Video>
-      </section>
     </>
   );
 };
